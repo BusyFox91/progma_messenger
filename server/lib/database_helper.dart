2082e0c16619884eb4cab1b92228e764f6732276
BusyFox <a.lisin00@mail.ru>
2026-08-07 01:28:30 +0300

    -make initTeables here; -add 2 more tables

--- server/lib/database_helper.dart
+++ server/lib/database_helper.dart
@@ -5,27 +5,35 @@ import 'package:path/path.dart' as path;
 
 import 'package:server/database_users_helper.dart' as lib_users;
 import 'package:server/database_sessions_helper.dart' as lib_sessions;
+import 'package:server/database_groups_helper.dart' as lib_groups;
+import 'package:server/database_group_members_helper.dart' as lib_group_members;
 
 class DatabaseHelper {
   final Database _db;
   late final lib_users.Users users;
   late final lib_sessions.Sessions sessions;
+  late final lib_groups.Groups groups;
+  late final lib_group_members.GroupMembers groupMembers;
   DatabaseHelper(this._db) {
     _configureDatabase();
+    _initTables();
 
     users = lib_users.Users(_db);
     sessions = lib_sessions.Sessions(_db);
-
-    // _initTables();
+    groupMembers = lib_group_members.GroupMembers(_db);
+    groups = lib_groups.Groups(_db, groupMembers);
   }
 
-  void _configureDatabase() {
-    // Обязательно для работы ON DELETE CASCADE и констрейнтов
-    _db.execute('PRAGMA foreign_keys = ON;');
-  }
-
+  void _configureDatabase() { _db.execute('PRAGMA foreign_keys = ON;'); }
+  
   // init tables that not initialised 
-  // void _initTables() {}
+  void _initTables() {
+    lib_users.Users.initTable(_db);
+    lib_sessions.Sessions.initTable(_db);
+    lib_groups.Groups.initTable(_db);
+    lib_group_members.GroupMembers.initTable(_db);
+  }
+
   void close() { _db.close(); }
 }
 
