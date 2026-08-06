f7d2a389e5682ad042b1d513f34b2e3f1cd073a0
BusyFox <a.lisin00@mail.ru>
2026-08-07 01:27:47 +0300

    -refactor; -add group_members table to logic; -add deleteEmpty

--- server/lib/database_groups_helper.dart
+++ server/lib/database_groups_helper.dart
@@ -1,33 +1,62 @@
-//database_groupss_helper.dart
+//database_groups_helper.dart
 import 'package:sqlite3/sqlite3.dart';
+
+import 'package:server/database_group_members_helper.dart' as lib_group_members;
 
 class Groups {
   final Database _db;
+  final lib_group_members.GroupMembers _groupMembers;
 
-  Groups(this._db) {
-    _initTable();
-  }
+  Groups(this._db, this._groupMembers);
 
-  void _initTable() {
-    _db.execute('''
+  static void initTable(Database db) {
+    db.execute('''
       CREATE TABLE IF NOT EXISTS groups (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        creator_id TEXT,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        type TEXT NOT NULL DEFAULT 'group' CHECK(type IN ('direct', 'group')),
+        name TEXT NULL,              -- NULL if type == 'direct'
+        creator_id INTEGER NULL,     -- NULL if type == 'direct'
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        empty_since DATETIME NULL,
         FOREIGN KEY (creator_id) REFERENCES users (id) ON DELETE SET NULL
       );
     ''');
   }
 
-  bool create(String groupName, int creatorId) {
+
+  bool create(String? groupName, int? creatorId, String type) {
+    if (groupName == '') {
+      print('Error. Can\'t create group with empty name');
+      return false;
+    }
+    if (type == 'group') {
+      if (groupName == null) {
+        print('Error. Can\'t create group without name');
+        return false;
+      }
+      if (creatorId == null) {
+        print('Error. Can\'t create group without creator');
+        return false;
+      }
+    }
     final stmt = _db.prepare(
-      'INSERT INTO groups (name, creator_id) VALUES (?, ?, ?)'
+      'INSERT INTO groups (name, creator_id, type) VALUES (?, ?, ?)'
     );
     try {
-      stmt.execute([groupName, creatorId]);
+      _db.execute('BEGIN TRANSACTION;');
+
+      stmt.execute([groupName, creatorId, type]);
+      
+      if (creatorId != null) {
+        final int newGroupId = _db.lastInsertRowId;
+        final bool memberCreationResult = _groupMembers.create(newGroupId, creatorId, 'creator');
+        if (!memberCreationResult) {throw Exception('Fail to create member \'creator\' to the group $newGroupId');}
+      }
+      _db.execute('COMMIT;');
       return true;
     } catch(e) {
+      _db.execute('ROLLBACK;');
+
       print('Error. Group creation error:\n$e');
       return false;
     } finally {
@@ -35,7 +64,7 @@ class Groups {
     }
   }
 
-  bool deleteGroup(int groupId) {
+  bool delete(int groupId) {
     final stmt = _db.prepare(
       'DELETE FROM groups WHERE id = ?'
     );
@@ -56,6 +85,18 @@ class Groups {
     }
   }
 
+  int deleteEmpty() {
+    try {
+      _db.execute(
+        'DELETE FROM groups WHERE empty_since IS NOT NULL AND empty_since < datetime(\'now\', \'-30 days\')',
+      );
+      return _db.updatedRows;
+    } catch(e) {
+      print('Error. Deletion empty groups error:\n$e');
+      return -1;
+    }
+  }
+  
   Row? getById(int groupId) {
     final stmt = _db.prepare(
       'SELECT * FROM groups WHERE id = ? LIMIT 1'
@@ -84,12 +125,12 @@ class Groups {
       print('Warning. Group\'s name wasn\'t changed. Old group\'s name matches new');
       return false;
     }
+
     final stmt = _db.prepare(
       'UPDATE groups SET name = ? WHERE id = ?',
     );
     try {
       stmt.execute([newGroupname, groupId],);
-
       return true;
     } catch(e) {
       print('Error. Changing group\'s name error:\n$e');
@@ -124,6 +165,5 @@ class Groups {
       stmt.close();
     }
   }
-
   
 }
